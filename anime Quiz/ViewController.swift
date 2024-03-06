5930e2839a4b8fae73185aca89d46abc2fb959e1
soshi212419 <shiuso1009@icloud.com>
2024-03-07 00:50:31 +0900

    Initial Commit

---
+++ anime Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  anime Quiz
+//
+//  Created by USER on 2024/03/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
